f8337cc17cebbe1845ffb1a532aa6c436da97e55
Swaraj <swarajunde1@gmail.com>
2026-05-29 01:10:43 +0530

    Add validation middleware and user registration validator using express-validator

---
+++ src/validators/index.js
@@ -0,0 +1,23 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+    return [
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("Email is Required")
+            .isEmail.withMessage("Email is Invalid"),
+        body("username")
+            .trim()
+            .notEmpty()
+            .withMessage("Username is Required")
+            .isLowercase()
+            .withMessage("Must be in lowercase")
+            .isLength({ min: 3 })
+            .withMessage("Username must be atleast of length 3"),
+        body("password").trim().notEmpty().withMessage("Password is required"),
+        body("fullName").optional().trim(),
+    ];
+};
+
+export { userRegisterValidator };
