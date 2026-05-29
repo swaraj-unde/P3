698ac8f88f85dc5dcb0d83813ac55638add3b076
Swaraj <swarajunde1@gmail.com>
2026-05-29 22:38:24 +0530

    Add logout functionality to auth controller and routes

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -122,4 +122,29 @@ const loginUser = asyncHandler(async (req, res) => {
         );
 });
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {
+                refreshToken: 1,
+            },
+        },
+        {
+            new: true,
+        },
+    );
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User LoggedOut successfully!"));
+});
+
+export { registerUser, loginUser, logoutUser };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,14 +1,20 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/auth.controllers.js";
+import {
+    loginUser,
+    logoutUser,
+    registerUser,
+} from "../controllers/auth.controllers.js";
 import { validate } from "../middleware/validator.middleware.js";
 import {
     userLoginValidator,
     userRegisterValidator,
 } from "../validators/index.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
