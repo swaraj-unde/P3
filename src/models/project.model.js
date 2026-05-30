e5e8fc7dd03f6204b38df15c310227abccee3e59
Swaraj <swarajunde1@gmail.com>
2026-05-30 12:27:51 +0530

    Project Model Added

---
+++ src/models/project.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const projectSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+export const Project = mongoose.model("Project", projectSchema);
